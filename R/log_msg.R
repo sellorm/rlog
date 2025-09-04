750c7ed10d7e417be96de8928b43fcd7247107d0
Mark Sellors <git@5vcc.com>
2025-09-04 10:07:17 +0100

    Improves codes readability

--- R/log_msg.R
+++ R/log_msg.R
@@ -1,5 +1,5 @@
 # Main log writing function
-log_msg <- function(msg_level = "INFO", message, ...){
+log_msg <- function(msg_level = "INFO", message, ...) {
   level <- Sys.getenv("LOG_LEVEL", "INFO")
   levels <- list(
     "all" = 0,
@@ -11,18 +11,24 @@ log_msg <- function(msg_level = "INFO", message, ...){
     "fatal" = 6,
     "off" = 7
   )
-  if (is.null(levels[tolower(level)])){
+  if (is.null(levels[tolower(level)])) {
     stop("The LOG_LEVEL environment variable is incorrectly set")
   }
   if (as.integer(levels[tolower(level)]) <= as.integer(levels[tolower(msg_level)])) {
+    log_text <- paste0(Sys.time(),
+      " [", msg_level, "] ",
+      paste(c(message, list(...)), sep = " ", collapse = " "),
+      "\n",
+      sep = "",
+      collapse = ""
+    )
     if (as.integer(levels[tolower(msg_level)]) > 3) {
-      cat(paste0(Sys.time(), " [", msg_level, "] ", paste(message, ...),  "\n"), file=stderr())
+      cat(log_text, file = stderr())
     } else {
-      cat(paste0(Sys.time(), " [", msg_level, "] ", paste(message, ...),  "\n"), file=stdout())
+      cat(log_text, file = stdout())
     }
     invisible(TRUE)
   } else {
     invisible(FALSE)
   }
 }
-
